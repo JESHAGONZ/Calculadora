25e6fc607e7110c3929f9188631ce24b677923f3
HEMMER JESHAIT GONZALEZ SANCHEZ <132520994+JESHAGONZ@users.noreply.github.com>
2023-11-06 17:52:31 -0600

    Add files via upload

---
+++ java.js
@@ -0,0 +1,29 @@
+    function sumar() {
+    let n1, n2, sumar
+    n1 = prompt ("introduce un numero")
+    n2 = prompt ("introduce otro numero")
+    sumar = parseInt (n1) + parseInt (n2)
+alert (sumar)
+}
+    function restar() {
+    let n1, n2, restar
+    n1 = prompt ("introduce un numero")
+    n2 = prompt ("introduce otro numero")
+    restar = parseInt (n1) - parseInt (n2)
+alert (restar)
+}
+    function multiplicar(){
+    let n1, n2, multiplicar
+    n1 = prompt ("introduce un numero")
+    n2 = prompt ("introduce otro numero")
+    multiplicar = parseInt(n1) * parseInt (n2)
+alert (multiplicar)
+    }
+    
+    function dividir() {
+    let n1, n2, dividir
+    n1 = prompt ("introduce un numero")
+    n2 = prompt ("introduce otro numero")
+    dividir = parseInt(n1) / parseInt(n2)
+alert (dividir)
+    }
